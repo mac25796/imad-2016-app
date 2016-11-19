ae57400ce5d915d29b403aa1b8da8b1178ea10a0
Muthu Annamalai Chidambaram <muthuannamalai14058@cse.ssn.edu.in>
2016-11-19 16:37:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -126,7 +126,7 @@ function loadArticles () {
                 var content = '<ul>';
                 var articleData = JSON.parse(this.responseText);
                 for (var i=0; i< articleData.length; i++) {
-                    content += `<li>
+                    content += `<li class="art">
                     <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
                     (${articleData[i].date.split('T')[0]})</li>`;
                 }
